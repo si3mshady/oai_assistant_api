768ed8c839e65ba91f5b6a93b3772f6dfa7d0151
Elliott Arnold <si3mshady@Elliotts-MBP.lan>
2023-12-14 17:03:33 -0600

    save point

--- vanilla_assistant.py
+++ vanilla_assistant.py
@@ -1,93 +1,77 @@
 import os, shelve
 from dotenv import load_dotenv
 from openai import OpenAI
+import time
+
 
 
 class OpenAIAssistant:
-    def __init__(self, api_key=None, name="MrRobot", \
-        instructions="Provide an answer for all questions", tools=None, model="gpt-3.5-turbo"):
+    def __init__(self, assistant_name="MrRobot", \
+        instructions="Provide an answer for all questions",  model="gpt-3.5-turbo"):
+        #uses a .env file with OPENAI_API_KEY variable 
         load_dotenv()
         self.client = OpenAI()
         self.thread_id = None #updated with self.create_thread
-        self.name = name
+        self.name = assistant_name
         self.instructions = instructions
-        self.tools = tools
         self.model = model
         self.assistant = None #updated with self.create(assistant)
-        self.active_messages = []
         # self.message = "Do your best to answer the question"
-        self.create_prompt_message = "When Is Veterans Day each year in America?"
+        # self.create_prompt_message = "When Is Veterans Day each year in America?"
     
-    def run_errand(self, thread_id, assistant_id ,instructions):
+    def run_errand_get_messages(self, thread_id, assistant_id ,instructions):
         kwargs = {
             "thread_id": thread_id,
             "assistant_id": assistant_id,
             "instructions": instructions
         }
         run = self.client.beta.threads.runs.create(**kwargs)
-        import time
-        while run.status == 'queued':
+
+        while run.status != 'completed':
             time.sleep(5)
             run = self.client.beta.threads.runs.retrieve(
             thread_id=thread_id,
             run_id=run.id
             )
+            
         messages = self.client.beta.threads.messages.list( thread_id=thread_id )
         print("\n\n" + messages.data[0].content[0].text.value + "\n\n")
+        return messages
         
       
 
-    def create_message(self, message):
-        id = self.get_thread_id()
-        if not id:
-            self.create_thread()
-        else:
-            id =  self.get_thread_id()
+    def create_message(self,thread_id, message):        
+        try: 
+            data = {"thread_id": thread_id, "role": "user", "content": message}
+            message = self.client.beta.threads.messages.create(**data)
 
-        data = {"thread_id": id, "role": "user", "content": message}
+        except Exception as e:
+            print(str(e))
 
-        message = self.client.beta.threads.messages.create(**data)
         
         
 
-    # def get_thread_id(self):
-    #     with shelve.open('db') as db:
-    #          # Retrieve the value associated with the key
-    #         value = db[self.name]
-    #         # Do something with the value
-    #         self.thread_id = value
-    #         return value
-
-    # def save_to_db(self, key, value):
-    #     with shelve.open('db') as db:
-    #         db[key] = value
-
-    # def check_thread_exists(self,key):
-    #     with shelve.open('db') as db:
-    #         if key in db:
-    #             return True
-    #         pass
-    
-    def create_assistant(self):
+    def create_assistant_on_init(self):
     
         kwargs = {"name": self.name, "instructions": self.instructions, "model": self.model}
         try:
             assistant = self.client.beta.assistants.create(**kwargs)
             print(assistant)
             self.assistant =  assistant           
+            return self.assistant
 
         except Exception as e:
             print(str(e))
+            return str(e)
 
     def create_thread(self):
-        result = self.check_thread_exists(self.name)
-        if not result: 
+        try:           
             thread = self.client.beta.threads.create()
-            self.save_to_db(self.name, thread.id)
             self.thread_id = thread.id
-            print(self.thread_id)
-            print('new thread saved to db')
-            return thread.id 
+            return thread
+        except Exception as e:
+            print(str(e))
+
 
        
         
